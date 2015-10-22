085dac197cc21cf477e1fdf792dd4aa96269adb7
Manu <steele07@hotmail.com>
2015-10-22 14:38:01 +0200

    Create folio.php

---
+++ folio.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+
+<html lang="fr">
+
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="initial-scale=1, user-scalable=yes, maximum-scale=1">
+		<meta name="author" content="Manuel Vercucque">	
+		<title>ManuelWeb</title>
+		<!--[if lt IE 9]>
+        <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    	<![endif]-->
+    	<link rel="shortcut icon" type="image/x-icon" href="images/mfavicon.ico" />
+		<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+    	<link href="index.css" rel="stylesheet" />	
+	</head>
+
+	<body>
+	<main>
+		<header id="header">	
+	        <nav class="navbar navbar-default">
+				<div class="container">					
+					<div class="navbar-header">
+						<a href="index.php"><span class="logo">.M.</span>
+						<span class="intit">Bienvenue sur ManuelWeb.fr</span></a>
+
+	                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+	                    <span class="sr-only">Toggle navigation</span>
+	                    <span class="icon-bar"></span>
+	                    <span class="icon-bar"></span>
+	                    <span class="icon-bar"></span>
+	                    </button>
+					</div>
+					<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+	                    <ul class="nav navbar-nav">
+							<li class="li-home"><a href="index.php"><i class="fa fa-home fa-lg"></i><br />Accueil</a></li>
+							<li class="li-about"><a href="index.php#about"><i class="fa fa-user fa-lg"></i><br />A propos</a></li>
+							<li class="li-skills"><a href="index.php#skills"><i class="fa fa-code fa-lg"></i><br />Compétences</a></li>
+							<li class="li-folio"><a href="folio.php"><i class="fa fa-briefcase fa-lg"></i><br />Portfolio</a></li>	
+							<li class="li-contact"><a href="index.php#contact"><i class="fa fa-envelope fa-lg"></i><br />Contact</a></li>							
+						</ul>						
+					</div>										
+				</div>
+			</nav>
